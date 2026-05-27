429a2136c1c67e56e6203777bf293912850800f4
GodIuz <notyetv@gmail.com>
2026-05-27 06:13:01 +0300

    solving ui issue

--- src/app/error404/error404.ts
+++ src/app/error404/error404.ts
@@ -1,8 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-error404',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './error404.html',
   styleUrl: './error404.css',
 })
