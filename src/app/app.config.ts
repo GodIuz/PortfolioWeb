19bc67c81bba24ffa2eaa5951cd9f5e66e302363
GodIuz <notyestv@gmail.com>
2026-06-02 05:34:31 +0300

    refactor app configuration for improved translation handling and initialization

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,36 +1,41 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, APP_INITIALIZER } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideTranslateService, TranslateService } from '@ngx-translate/core';
-import { provideTranslateHttpLoader, TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { lastValueFrom } from 'rxjs';
-
-export function HttpLoaderFactory() {
-  return new TranslateHttpLoader();
-}
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
+import { lastValueFrom, catchError, of } from 'rxjs';
 
 export function initApp(translate: TranslateService) {
   return () => {
     translate.addLangs(['en', 'el']);
     translate.setDefaultLang('en');
+    
     const browserLang = translate.getBrowserLang();
     const langToUse = browserLang?.match(/el/) ? 'el' : 'en';
-    return lastValueFrom(translate.use(langToUse));
+    return lastValueFrom(
+      translate.use(langToUse).pipe(catchError(() => of(null)))
+    );
   };
 }
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withFetch()), 
     provideTranslateService({
-      defaultLanguage: 'en'
+      fallbackLang: 'en'
     }),
     provideTranslateHttpLoader({
       prefix: './i18n/',
       suffix: '.json'
-    })
+    }),
+
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initApp,
+      deps: [TranslateService],
+      multi: true
+    }
   ]
 };
